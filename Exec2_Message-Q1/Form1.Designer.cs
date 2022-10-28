8d1ed9f5c32a602ae5f42cfaf338f31cf896d05e
ispan <ispan@DESKTOP-PAKSETB>
2022-10-28 16:55:38 +0800

    加入專案檔案。

---
+++ Exec2_Message-Q1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Exec2_Message01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(137, 211);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "打招呼";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(135, 130);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(317, 61);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "顯示時間 跟問候語";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "目前時間";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.showButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showButton;
+        public System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Exec2_Message-Q2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Message02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            resultLabel.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime datetime = DateTime.Now;
+            int hours = datetime.Hour;
+
+
+            string result = GetHello(hours);
+
+            resultLabel.Text = result +"\r\n"+ $"目前時間:{datetime}";
+
+        }
+
+        private string GetHello(int hours)
+        {
+            string value = string.Empty;
+            if (hours < 12)
+            {
+                value = "您好，早安";
+            }
+            else if (hours > 12)
+            {
+                value = "您好，晚安";
+            }
+            else
+            {
+                value = "您好，午安";
+            }
+
+            return value;
+
+        }
+    }
+    
+}
